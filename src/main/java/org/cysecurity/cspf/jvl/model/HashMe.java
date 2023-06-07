728ff239aa1d13e92d760d3894331c7bc7da60b9
Nidhi Jaiswal <35574348+nidhi0512@users.noreply.github.com>
2023-06-07 13:53:32 +0530

    Update HashMe.java

--- src/main/java/org/cysecurity/cspf/jvl/model/HashMe.java
+++ src/main/java/org/cysecurity/cspf/jvl/model/HashMe.java
@@ -12,20 +12,14 @@ public class HashMe {
     {
          StringBuffer sb=null;
         try
-        {   
-        SecretKey key = new SecretKeySpec(SECRET_KEY_FROM_CONFIG, "MD5");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            // Consider a stronger mode, such as GCM / CCM / or other AEAD mode, if required
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        encData = cipher.doFinal(plainText.getBytes("UTF-8"));
-        byte[] iv = cipher.getIV();
-          //  MessageDigest md = MessageDigest.getInstance("MD5");
-          //  md.update(str.getBytes());
-          //  byte byteData[] = md.digest();
-           sb= new StringBuffer();
-            for (int i = 0; i < iv.length; i++) 
+         {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte byteData[] = md.digest();
+            sb= new StringBuffer();
+            for (int i = 0; i < byteData.length; i++)  
             {
-             sb.append(Integer.toString((iv[i] & 0xff) + 0x100, 16).substring(1));
+             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
             }   
         }
         catch(NoSuchAlgorithmException e)
